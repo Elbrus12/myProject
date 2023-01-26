35b84ef463956860a368ffcf4e0af0b0d861afbc
Elbrus12 <Guliev.elbrus1@yandex.ru>
2023-01-26 23:26:14 +0300

    newprojects

---
+++ main.py
@@ -0,0 +1,10 @@
+from win32com.client import Dispatch
+
+
+def create_shortcut(file_name: str, target: str, work_dir: str, arguments: str = ''):
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(file_name)
+    shortcut.TargetPath = target
+    shortcut.Arguments = arguments
+    shortcut.WorkingDirectory = work_dir
+    shortcut.save()
