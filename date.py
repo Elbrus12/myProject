6e78217a25cf79bceee07f126e3df366b3ef09bd
Elbrus12 <Guliev.elbrus1@yandex.ru>
2023-03-06 23:33:29 +0300

    Поместил все в один файл, добавил возможность вводить сайт и ставить название самостоятельно. Все поместил, и сделал в виде функции

--- date.py
+++ date.py
@@ -1,5 +1,1 @@
-import random
-
-site = "https://vk.com/elbrus_offical"
-name = str(random.random()) + ".png"
 
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import qrcode
-import date
 import os
-# пример данных можно отредактировать в date.py
-data = date.site
-# имя конечного файла
-filename = date.name
-# создаем директорию для сохранения
-os.chdir('qrcodes')
-# генерируем qr-код
-img = qrcode.make(data)
-# сохраняем img в файл
-img.save(filename)
+
+def makeQR():
+    site = input('Введите точный URL-адрес или нужную вам вещь: ')  # Пишем сайт или различные вещи для QR
+    name = str(input('Как хотите назвать изображение: ')) + ".png"  # Генерация имени для изображения
+    # создаем директорию для сохранения
+    dir_name = 'qrcodes'
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    # пример данных можно отредактировать в date.py
+    data = site
+    # имя конечного файла
+    filename = name
+    # генерируем qr-код
+    img = qrcode.make(data)
+    # переходим в данную директорию
+    fullpath = os.path.join(dir_name, filename)
+    #сохраняем файл
+    img.save(fullpath)
+
+makeQR()
